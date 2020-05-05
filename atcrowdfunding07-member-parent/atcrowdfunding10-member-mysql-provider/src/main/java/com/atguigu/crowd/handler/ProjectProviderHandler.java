85354ef6e187ce45f7ec222a862193d2929dff0d
huanglang777 <huanglang777@outlook.com>
2020-05-06 00:12:18 +0800

    show project detail

--- atcrowdfunding07-member-parent/atcrowdfunding10-member-mysql-provider/src/main/java/com/atguigu/crowd/handler/ProjectProviderHandler.java
+++ atcrowdfunding07-member-parent/atcrowdfunding10-member-mysql-provider/src/main/java/com/atguigu/crowd/handler/ProjectProviderHandler.java
@@ -1,20 +1,37 @@
 package com.atguigu.crowd.handler;
 
+import com.atguigu.crowd.entity.vo.DetailProjectVO;
+import com.atguigu.crowd.entity.vo.PortalTypeVO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.atguigu.crowd.entity.vo.ProjectVO;
 import com.atguigu.crowd.service.api.ProjectService;
 import com.atguigu.crowd.util.ResultEntity;
+
+import java.util.List;
 
 @RestController
 public class ProjectProviderHandler {
 	
 	@Autowired
 	private ProjectService projectService;
+
+	@RequestMapping("/get/project/detail/remote/{projectId}")
+	public ResultEntity<DetailProjectVO> getDetailProjectVORemote(@PathVariable("projectId") Integer projectId) {
+
+		try {
+			DetailProjectVO detailProjectVO = projectService.getDetailProjectVO(projectId);
+
+			return ResultEntity.successWithData(detailProjectVO);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResultEntity.failed(e.getMessage());
+		}
+
+	}
 	
 	@RequestMapping("/save/project/vo/remote")
 	public ResultEntity<String> saveProjectVORemote(
@@ -32,6 +49,21 @@ public class ProjectProviderHandler {
 			return ResultEntity.failed(e.getMessage());
 		}
 		
+	}
+
+	@RequestMapping("/get/portal/type/project/data/remote")
+	public ResultEntity<List<PortalTypeVO>> getPortalTypeProjectDataRemote() {
+
+		try {
+			List<PortalTypeVO> portalTypeVOList = projectService.getPortalTypeVO();
+
+			return ResultEntity.successWithData(portalTypeVOList);
+		} catch (Exception e) {
+			e.printStackTrace();
+
+			return ResultEntity.failed(e.getMessage());
+		}
+
 	}
 
 }
--- atcrowdfunding07-member-parent/atcrowdfunding10-member-mysql-provider/src/main/java/com/atguigu/crowd/mapper/ProjectPOMapper.java
+++ atcrowdfunding07-member-parent/atcrowdfunding10-member-mysql-provider/src/main/java/com/atguigu/crowd/mapper/ProjectPOMapper.java
@@ -3,6 +3,9 @@ package com.atguigu.crowd.mapper;
 import com.atguigu.crowd.entity.po.ProjectPO;
 import com.atguigu.crowd.entity.po.ProjectPOExample;
 import java.util.List;
+
+import com.atguigu.crowd.entity.vo.DetailProjectVO;
+import com.atguigu.crowd.entity.vo.PortalTypeVO;
 import org.apache.ibatis.annotations.Param;
 
 public interface ProjectPOMapper {
@@ -35,4 +38,8 @@ public interface ProjectPOMapper {
 	void insertTagRelationship(
             @Param("tagIdList") List<Integer> tagIdList,
             @Param("projectId") Integer projectId);
+
+    List<PortalTypeVO> selectPortalTypeVOList();
+
+    DetailProjectVO selectDetailProjectVO(Integer projectId);
 }
